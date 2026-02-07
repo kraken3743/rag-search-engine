f02f0a0d888f6551702780aeff3ed11985924363
Shashwat Amrit <shashwatamrit@Shashwats-MacBook-Air.local>
2026-02-07 22:44:01 +0530

    tokenization

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -4,15 +4,27 @@ def clean_text(text):
       text=text.lower()
       text=text.translate(str.maketrans("","", string.punctuation))
       return text
+
+def tokenize_text(text):
+      text = clean_text(text)
+      tokens = [tok for tok in text.split() if tok]
+      return tokens
+def has_matching_tokens(query_tokens,movie_tokens):
+        for query_tok in query_tokens:
+            for movie_tok in movie_tokens:
+                  if query_tok in movie_tok:
+                        return True
+        return False
       
 def search_command(query, n_results):
         movies = load_movies()
         res = []
-        query = clean_text(query)
+        query_tokens = tokenize_text(query)
         for movie in movies:
-                if query in clean_text (movie['title']):
+                movie_tokens = tokenize_text(movie['title'])
+                if has_matching_tokens(query_tokens, movie_tokens):
                     res.append(movie)
                 if len(res) == n_results:
                     break
         return res 
-        pass
+        
