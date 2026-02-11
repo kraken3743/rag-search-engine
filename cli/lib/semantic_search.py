dad2570ac34fa1d5f783909fa371beade321f6fa
Shashwat Amrit <shashwat813@gmail.com>
2026-02-11 19:12:52 +0530

    Generating Text Embeddings

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -6,6 +6,19 @@ load_dotenv()
 class SemanticSearch:
     def __init__(self):
         self.model = SentenceTransformer('all-MiniLM-L6-v2')
+    
+    def generate_embedding(self, text):
+        if not text or not text.strip():
+            raise ValueError("Input text cannot be empty or whitespace")
+        return self.model.encode([text])[0]
+
+
+def embed_text(text):
+    ss = SemanticSearch()
+    embedding = ss.generate_embedding(text)
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
 
 def verify_model():
     ss = SemanticSearch()
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 
 import argparse
-from lib.semantic_search import (verify_model)
+from lib.semantic_search import (
+    verify_model,
+    embed_text
+    )
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
     
     subparsers.add_parser("verify", help="Verify the Embedding Model loads properly")
-    
+
+    embed_parser = subparsers.add_parser("embed_text", help="Encode text with embedding model ")
+    embed_parser.add_argument("text", type=str, help="text to be encoded")
     args = parser.parse_args()
 
     match args.command:
+        case "embed_text":
+            embed_text(args.text)
         case "verify":
             verify_model()
         
