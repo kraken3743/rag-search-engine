c8018509020386066f086463db0d333975f18693
Shashwat Amrit <shashwat813@gmail.com>
2026-02-11 22:07:56 +0530

    Document embedding

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,17 +1,52 @@
 from dotenv import load_dotenv
 from sentence_transformers import SentenceTransformer
+import numpy as np
+from pathlib import Path
+from lib.search_utils import load_movies
 
 load_dotenv()
 
 class SemanticSearch:
     def __init__(self):
         self.model = SentenceTransformer('all-MiniLM-L6-v2')
+        self.embeddings = None
+        self.documents = None
+        self.document_map = {}
+        self.embeddings_path = Path("cache/movie_embeddings.npy")
+
+    def build_embeddings(self, documents):
+        self.documents = documents
+        self.document_map = {}
+        movie_strings = []
+        for doc in self.documents:
+            self.document_map[doc['id']] = doc #in the document map, put the doc itself as the value, and the id as the key
+            movie_strings.append(f"{doc['title']} {doc['description']}")
+        self.embeddings = self.model.encode(movie_strings, show_progress_bar=True)
+        np.save('cache/movie_embeddings.npy', self.embeddings)
+        return self.embeddings
     
+    def load_or_create_embeddings(self, documents):
+        self.documents = documents
+        self.document_map = {}
+        for doc in self.documents:
+            self.document_map[doc['id']] = doc
+        if self.embeddings_path.exists():
+            self.embeddings = np.load(self.embeddings_path)
+            if len(self.documents) == len(self.embeddings):
+                return self.embeddings
+        return self.build_embeddings(documents)
+                
     def generate_embedding(self, text):
         if not text or not text.strip():
             raise ValueError("Input text cannot be empty or whitespace")
         return self.model.encode([text])[0]
 
+def verify_embedding():
+    ss = SemanticSearch()
+    documents = load_movies()  
+    embeddings = ss.load_or_create_embeddings(documents)
+    print(f"Number of docs:   {len(documents)}")
+    print(f"Embeddings shape: {embeddings.shape[0]} vectors in {embeddings.shape[1]} dimensions")
 
 def embed_text(text):
     ss = SemanticSearch()
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -3,8 +3,9 @@
 import argparse
 from lib.semantic_search import (
     verify_model,
-    embed_text
-    )
+    embed_text,
+    verify_embedding
+)
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
@@ -14,9 +15,14 @@ def main():
 
     embed_parser = subparsers.add_parser("embed_text", help="Encode text with embedding model ")
     embed_parser.add_argument("text", type=str, help="text to be encoded")
+
+    embed_parser = subparsers.add_parser("verify_embeddings", help="verify")
+
     args = parser.parse_args()
 
     match args.command:
+        case "verify_embeddings":
+            verify_embedding()
         case "embed_text":
             embed_text(args.text)
         case "verify":
