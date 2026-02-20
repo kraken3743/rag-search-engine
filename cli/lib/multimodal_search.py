d8952092b58ef648b0583def87a70ab34f43f46f
Shashwat Amrit <shashwat813@gmail.com>
2026-02-20 23:01:33 +0530

    Multimodal Search Implementation

--- cli/lib/multimodal_search.py
+++ cli/lib/multimodal_search.py
@@ -1,16 +1,52 @@
 from PIL import Image
 from sentence_transformers import SentenceTransformer
+from lib.semantic_search import cosine_similarity
+from lib.search_utils import load_movies 
 
 class MultiModalSearch:
-    def __init__(self, model_name="clip-ViT-B-32"):
+    def __init__(self, documents, model_name="clip-ViT-B-32"):
         self.model = SentenceTransformer(model_name)
+        self.documents = documents
+        self.texts = []
+        for doc in documents:
+            self.texts.append(f"{doc['title']}: {doc['description']}")
+        self.text_embeddings = self.model.encode(self.texts, show_progress_bar=True)
     
     def embed_image(self, image_fpath):
         img = Image.open(image_fpath)
         return self.model.encode([img])[0]
+    
+    def search_with_image(self, image_fpath, limit=5):
+        image_emb = self.embed_image(image_fpath)
+
+        similarities = []
+        for idx, text_emb in enumerate(self.text_embeddings):
+            similarities.append((idx, cosine_similarity(image_emb, text_emb)))
+
+        sorted_sims = sorted(similarities, key=lambda x: x[1], reverse=True)
+        sorted_sims = sorted_sims[:limit]
+        results = []
+        for (idx, score) in sorted_sims:
+            _doc = self.documents[idx]
+            results.append({
+                'title': _doc['title'],
+                'description': _doc['description'],
+                'doc_id': idx,
+                'score': score
+            })
+        return results
+
+def image_search_command(image_fpath, limit=5):
+    movies = load_movies()
+    ms = MultiModalSearch(movies)
+    res  = ms.search_with_image(image_fpath, limit)
+    for  i, r in enumerate(res, start=1):
+        print(f"{i}. {r['title']} (similarity: {r['score']:.3f})")
+        print(f"{r['description'][:100]}")  
 
 def verify_image_embedding(image_fpath):
-    ms = MultiModalSearch()
+    movies = load_movies
+    ms = MultiModalSearch(movies)
     embedding = ms.embed_image(image_fpath)
     print(f"Embedding shape: {embedding.shape[0]} dimensions")
 
--- cli/multimodal_search_cli.py
+++ cli/multimodal_search_cli.py
@@ -1,5 +1,5 @@
 import argparse
-from lib.multimodal_search import verify_image_embedding
+from lib.multimodal_search import verify_image_embedding, image_search_command
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Hybrid Search CLI")
@@ -8,9 +8,15 @@ def main() -> None:
     verify_parser = subparsers.add_parser(name = "verify_image_embedding", help="Verify image embeddings load")
     verify_parser.add_argument('image_fpath', type=str, help="path to the image to process")
 
+    verify_parser = subparsers.add_parser(name = "image_search", help="Search for movies based on an image")
+    verify_parser.add_argument('image_fpath', type=str, help="path to the image to search with")
+    verify_parser.add_argument('--limit', type=int, default=5, help="Number of resulst to return")
+
     args = parser.parse_args()
 
     match args.command:
+        case "image_search":
+            image_search_command(args.image_fpath, args.limit)
         case "verify_image_embedding":
             verify_image_embedding(args.image_fpath)
 
